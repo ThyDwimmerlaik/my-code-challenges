fc2929b1e134075342c842519ebbbe604557898d
ThyDwimmerlaik <12750424+ThyDwimmerlaik@users.noreply.github.com>
2022-08-07 17:29:35 -0500

    Update of all typescript code

--- 1/typescript/src/SumInArray.test.ts
+++ 1/typescript/src/SumInArray.test.ts
@@ -7,11 +7,11 @@ describe("sumInArray function behaviour", () => {
     });
     
     test('verifies array with non existing sum', () => {
-        expect(sumInArray([50, 10, 15, 3, 6], 17)).toBe(false);
+        expect(sumInArray([50, 10, 15, 3, 6], 20)).toBe(false);
     });
 
     test('verifies array with less than 2 elements', () => {
         expect(sumInArray([17], 17)).toBe(false);
     });
     
-})
+});
