5df2b49948ced2f801287560ae9306aa2959a185
ThyDwimmerlaik <12750424+ThyDwimmerlaik@users.noreply.github.com>
2022-08-10 17:51:50 -0500

    Small optimization on challenge 3

--- 3/typescript/src/Cons.ts
+++ 3/typescript/src/Cons.ts
@@ -1,11 +1,7 @@
 type consType = (f: (a: number, b: number) => number ) => number;
 
-export const cons = (a: number, b: number): consType => {
-    const pair = (f: (a: number, b: number) => number ) => {
-        return f(a,b);
-    }
-    return pair;
-}
+export const cons = (a: number, b: number): consType => 
+    (f: (a: number, b: number) => number ) => f(a,b);
 
 export const car = (pair: consType): number => pair((a: number, b: number) => a);
 export const cdr = (pair: consType): number => pair((a: number, b: number) => b);
